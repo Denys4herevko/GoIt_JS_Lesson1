8e3776f332da09bf316df52bc1050860d22668ec
Denysssssss12 <112236035+Denysssssss12@users.noreply.github.com>
2023-01-13 21:35:15 +0200

    Summary

---
+++ js/Lesson1.js
@@ -0,0 +1,28 @@
+// 'Java script'.length
+// Скільки символів в рядку
+// name="Poly"
+// Операція присвоєння
+// friendName="Poly"
+// Кожне наступне слово з великої літери
+
+// let Number="10"
+
+// console.log(Привіт) виводить в консоль привіт
+// console.log(Nimber) виведе знечення змінної Number
+
+// Number = "15"
+// console.log(Number)
+// Видасть два значення
+
+// const age = 25 
+// Змінна яку неможно паереписувати
+// console.log(age)
+
+
+
+//    Взаємодія з коритувачем
+// console.log(window)
+// const message ='Ви впевнені що хочете покинути сторінку'
+// alert(message); Повідомлення 1 варант
+
+// confirm(message)
---
+++ js/Lesson1_Hw.js
@@ -0,0 +1,83 @@
+// 1 
+const str = 'Привіт!'
+const num = 123;
+const flag = true;
+const txt = 'true';
+
+console.log( typeof str);
+console.log( typeof num);
+console.log( typeof flag);
+console.log( typeof txt);
+
+// 2
+const age = 70;
+const Name='Valeriy';
+const areHere = true;
+const yourPassword = 1234567890;
+const youMan = true;
+
+let number = 105;
+let surname = 'Cherevko';
+let weight = '70';
+let isOnline = true;
+let time = 17.31;
+
+// 3
+const userName = prompt('Імя користувача');
+console.log('Імя користувача: ${userName}');
+
+// 4
+let myAge = 17;
+let myFriendAge = 17;
+alert( myAge + myFriendAge);
+
+// 5
+let a = 20;
+let b = 80 ;
+
+console.log(a+b);
+
+// 6
+let a1 = 5+3;
+
+console.log(a1);
+
+let a2 = 5-3;
+
+console.log(a2);
+
+let a3 = 5*3;
+
+console.log(a3);
+
+let a4 = 5/3;
+
+console.log(a4);
+
+let a5 = 5/3;
+
+console.log(a5);
+
+// 7
+let a6 = 5%3;
+alert( a6);
+let a7 = 3%5;
+alert( a7);
+let a8 = 5+'3';
+alert( a8);
+let a9 = '5'-3;
+alert( a9);
+let a10 = 75 +'кг';
+alert( a10);
+
+// 8
+const height = 23;
+const width = 10;
+const s=height*width;
+console.log(s);
+
+// 9
+const  heightC = 10;
+const dC = 4
+const v = heightC*dC
+console.log(v)
