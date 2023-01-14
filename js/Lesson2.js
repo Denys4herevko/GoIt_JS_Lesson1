8ba30eb4fccd7c3607b242894c5db4449ab111f5
Denysssssss12 <112236035+Denysssssss12@users.noreply.github.com>
2023-01-14 13:02:27 +0200

    Summary

---
+++ js/Lesson2.js
@@ -0,0 +1,76 @@
+//  const num = 5;
+
+
+
+// console.dir(Number)
+// let elementWidth = '50px';
+// Number.parseInt(elementWidth)
+// let elementHeight = '200.74px'
+// consle.log(elementWidth)
+
+// let salary = 130.1672
+// salary.toFixed()
+// const res = Number(salary.toFixed())
+// console.log(salary.toFixed())
+// Відсіче все до крапки й видасть 130
+
+
+// const base = 2;
+// const power = 5;
+// const res =Math.pow(base,power)
+// console.log(res)
+// Зведення до степеню
+
+
+
+/*
+ * Напиши скрипт який просить у користувача ввести число і степінь,
+ * підводить число в цю степінь і виводить результат в консоль.
+ */
+// const num = prompt("Число")
+// const powe = prompt("Степінь")
+// console.log(Math.powe)
+
+
+// const random = Math.random() * (10 - 5) + 5
+// console.log(Math.round())
+
+// console.log('5' + 5)
+
+
+
+/*
+ * Напиши скрипт який виведе рядок в форматі:
+ * «Гість x y поселяється в g номер q»,
+ * підставив замість x y g q значення змінних
+ */
+// const firstName = 'Mango';
+// const lastName = 'Lee';
+// const room = 716;
+// const type = 'VIP';
+// const userData = "Гість" + firstName+""+lastName+"поселяєтся в"+room+"номер"+"type";
+// console.log(userData)
+
+
+// const quantity = 15;
+// const orderMsg = `Ви замовили ${quantity} тістечок.`
+// console.log(orderMsg)
+
+
+// const message = `В цьому рядку 26 символів`
+// console.log(message.toLocaleLowerCase())
+
+// const blacklistedWord1 = 'спам';
+// const blacklistedWord2 = 'розпродаж';
+
+// const string1 = 'Привіт, я принц Абдул, це не спам, пропоную тобі мільйон!';
+// const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+// const string3 = 'Рекламна кампанія #jsIsAwesome';
+// const norm=string1.toLowerCase()
+// const norm2=string2.toLowerCase
+// const norm3=string3.toLowerCase
+// console.log(norm2.inCludes(blacklistedWord1))
+// console.log(norm2.inCludes(blacklistedWord2))
+
+
+
