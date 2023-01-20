72850282ce043247cf311fbd18313937a93b6a93
Denysssssss12 <112236035+Denysssssss12@users.noreply.github.com>
2023-01-20 22:14:41 +0200

    Summary

--- js/Lesson2_HW.js
+++ js/Lesson2_HW.js
@@ -26,6 +26,23 @@
 // console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18)); 
 
 // 4
-// const number = Math.random(3,19);
-// console.log(number); !!
+// const number = Math.random()*18 + 3;
+// console.log(Number.parseInt(number));
+
+// 5
+// var email = 'sherevkodem@gmail.com';
+// console.log(email.length);
+// console.log(email.includes("@"));
+
+// 6
+// const word1 = 'My';
+// const word2 ='name';
+// const word3 = 'is';
+// const  fullName = word1+word2+word3+"Victor";
+// console.log(fullName);
+
+// 7
+// const userName ='Олександро';
+// const payment ='300 гривень';
+// alert('Дякуємо'+ userName+'!'+'До сплати'+ payment)
 
